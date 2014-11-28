c90a20da4884f5123bec8a8a1f4f6872443828fd
Anna Keren <anna.keren.me@gmail.com>
2014-11-28 13:52:37 +0200

    before submission

--- motionEstimation.m
+++ motionEstimation.m
@@ -1,36 +1,55 @@
 %motion estimation script
 % edit path 
+%path = 'C:\Users\aak33\AppData\MotionEstimation\DatasetC.mpg';
 path = 'C:\Users\Anna\Documents\GitHub\MotionEstimation\DatasetC.mpg';
 videoFrames = ICV_getFramesFromAVI(path);
-
+%4 a
 % block = zeros(16, 16);
 % searchWindow = zeros(20,20);
 
-
-% block = zeros(4, 4);
-% searchWindow = zeros(16,16);
-
-block = zeros(8, 8);
+%4 c
+block = zeros(4, 4);
 searchWindow = zeros(16,16);
 
+% block = zeros(8, 8);
+% searchWindow = zeros(16,16);
 
 % block = zeros(16, 16);
 % searchWindow = zeros(16,16);
+ 
+%4 d
+% block = zeros(8, 8);
+% searchWindow = zeros(8,8);
 
+% block = zeros(8, 8);
+% searchWindow = zeros(16,16);
+
+block = zeros(16, 16);
+searchWindow = zeros(16,16);
+
+% 
 % block = zeros(8, 8);
 % searchWindow = zeros(32,32);
 
-% block = zeros(4, 4);
-% searchWindow = zeros(8,8);
+
 
 
 %edit these variables with values of consequent frames
-frameK = 80;    %110 - 80 - 50 - 10 - 21 - 33
-frameKAnd1 = 81;%111 - 81 - 51 - 11 - 21 - 34
-[motionVectorUint8, predictionVectorUint8] = ICV_getMotionEstimation(videoFrames, frameK, frameKAnd1, block, searchWindow);
-% imshow(predictionVectorUint8)
-% uncomment this line to show motion vector
-% imshow(motionVectorUint8);
+frameK = 31;  
+frameKAnd1 = 32;
+%4 e, uncomment these lines to see performance report
+% profile on
 
-% uncomment this line to show prediction frame of k+1
-% imshow(predictionVectorUint8);
+[motionVectorUint8, predictionVectorUint8, xSource, ySource, uSource, vSource, frame1] = ICV_getMotionEstimation(videoFrames, frameK, frameKAnd1, block, searchWindow);
+%this show arrows on the frame
+figure;
+imshow(frame1, []);
+hold on;
+quiver(xSource, ySource, uSource, vSource);
+
+%4 e, uncomment these lines to see performance report
+% plot(magic(35))
+% profile viewer
+
+%4 b  , uncomment with 4 a
+%  imshow(predictionVectorUint8);
